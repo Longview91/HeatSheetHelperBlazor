20de0b1ac54b74d097a9ccf3bfff269feb9307c2
Steve Hansen <shansen91@gmail.com>
2025-06-02 22:28:17 -0500

    Made the theme color customizable. Next work on font sizes and color.

---
+++ Components/Pages/Settings.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeatSheetHelperBlazor.Components.Pages
+{
+    partial class Settings
+    {
+        [Inject] private NavigationManager Navigation { get; set; }
+        [Inject] private IJSRuntime JS { get; set; }
+        private string headerColor = "#8b0000"; // Default to darkred
+
+        protected override async Task OnInitializedAsync()
+        {
+            var color = await JS.InvokeAsync<string>("blazorLocalStorage.get", "tableHeaderColor");
+            if (!string.IsNullOrEmpty(color))
+                headerColor = color;
+        }
+
+        private async Task SaveColor()
+        {
+            await JS.InvokeVoidAsync("blazorLocalStorage.set", "tableHeaderColor", headerColor);
+            await JS.InvokeVoidAsync("setTableHeaderColor", headerColor);
+            Navigation.NavigateTo("/");
+        }
+
+        private async Task ResetColor()
+        {
+            headerColor = "#8b0000"; // Reset to default dark red
+            await JS.InvokeVoidAsync("blazorLocalStorage.set", "tableHeaderColor", headerColor);
+            await JS.InvokeVoidAsync("setTableHeaderColor", headerColor);
+            Navigation.NavigateTo("/");
+        }
+    }
+}
